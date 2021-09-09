8c6873b0972720cb4462e31bba360e9ba1f3a5fe
Leo-Chan01 <ephraimleo16@gmail.com>
2021-09-09 07:16:52 +0100

    Second Commit. Cleared out the Clutter 1

--- Requestor/Requestor.java
+++ Requestor/Requestor.java
@@ -5,21 +5,27 @@ import java.util.Scanner;
 class GetInputFromUser {
     public static void main(String args[])
     {
+         //This will take the name
         System.out.println("Welcome \n Input your name");
         Scanner nameScanner = new Scanner(System.in);
         String name = nameScanner.nextLine();
         System.out.println("Your name is : " + name);
+        // nameScanner.close();
 
+       //This will take the Reg No.
         System.out.println("Input your Reg No.");
         Scanner regNumberScanner = new Scanner(System.in);
         String regNumber =  regNumberScanner.nextLine();
-        System.out.println("Your name is : " + name);
+        System.out.println("Your Reg No. is : " + regNumber);v
+        // regNumberScanner.close();
 
+        //This will take the course
+    
         System.out.println("Input your course");
         Scanner courseScanner = new Scanner(System.in);
         String course = courseScanner.nextLine();
         System.out.println("Your your course is : " + course);
           // closing scanner
-          nameScanner.close();
+        courseScanner.close();
     }
 }
